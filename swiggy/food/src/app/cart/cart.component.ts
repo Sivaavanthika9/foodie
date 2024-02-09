46ea68be07fee05cc8e991668be1022b46cbc737
Sivaavanthika9 <siva.avanthika9@gmail.com>
2024-02-09 14:31:56 +0530

    first commit

---
+++ swiggy/food/src/app/cart/cart.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { AdminService } from '../shared/admin.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.scss'
+})
+export class CartComponent {
+  cartItems: any[] = [];
+  totalAmount: number = 0;
+
+  constructor(private cartService: AdminService) {}
+
+  ngOnInit(): void {
+    this.cartService.cartItems$.subscribe(items => {
+      this.cartItems = items;
+    });
+
+    this.cartService.totalAmount$.subscribe(amount => {
+      this.totalAmount = amount;
+    });
+  }
+
+}
